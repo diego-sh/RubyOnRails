66e0a753ec79a1c74b6acf2f6ddb2bd4761eb97a
Diego <diegoalpala91@gmail.com>
2017-03-02 23:30:55 -0500

    ok

--- ToaSystem2/config/routes.rb
+++ ToaSystem2/config/routes.rb
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
+
 Rails.application.routes.draw do
   resources :residencias
   resources :pacientes
-  get 'welcome/index'
-  root 'welcome#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
-=======
-Rails.application.routes.draw do
   resources :personas
-  resources :pacientes
   get 'welcome/index'
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
->>>>>>> origin/master
